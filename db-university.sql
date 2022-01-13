e73b558be3bb3aeca9f5db355e27be76a660db7c
elepot87 <eleonorapotente@gmail.com>
2022-01-13 17:26:43 +0100

    query 1

--- db-university.sql
+++ db-university.sql
@@ -1,3 +1,5 @@
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
-SELECT * FROM `students` WHERE `date_of_birth` BETWEEN '1990-01-01' AND '1990-12-31' 
+SELECT * 
+FROM `students` 
+WHERE `date_of_birth` BETWEEN '1990-01-01' AND '1990-12-31';
