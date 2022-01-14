3a69e081d0f49c533be8b9256993740854a8ab77
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-14 17:53:38 +0100

    GROUP_BY_query3

--- db_university_2.sql
+++ db_university_2.sql
@@ -9,4 +9,8 @@ FROM `teachers`
 GROUP BY `office_address`;
 
 -- 3.	Calcolare la media dei voti di ogni appello d'esame
+SELECT ROUND(AVG(`vote`), 0) AS `media_voto`, `exam_id` AS `esame`
+FROM `exam_student`
+GROUP BY `exam_id`;
+
 -- 4.	Contare quanti corsi di laurea ci sono per ogni dipartimento
