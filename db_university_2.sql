d5d310d0c664dce1eb8571f28c2017a2cdf2b6ca
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-14 17:42:20 +0100

    group_by_query2

--- db_university_2.sql
+++ db_university_2.sql
@@ -2,6 +2,11 @@
 SELECT COUNT(`id`) AS `numero_iscritti`, YEAR(`enrolment_date`) AS `anno`
 FROM `students` 
 GROUP BY YEAR(`enrolment_date`);
+
 -- 2.	Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(`id`) AS `num_insegnanti`, `office_address` AS `indirizzo_ufficio`
+FROM `teachers` 
+GROUP BY `office_address`;
+
 -- 3.	Calcolare la media dei voti di ogni appello d'esame
 -- 4.	Contare quanti corsi di laurea ci sono per ogni dipartimento
