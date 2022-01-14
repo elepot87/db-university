bd2ccb5ce2b72a228438bbbaa348f8cad8d36915
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-14 20:58:49 +0100

    join_query4

--- db_university_2.sql
+++ db_university_2.sql
@@ -36,7 +36,11 @@ SELECT *
 FROM `courses` 
 INNER JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id`
 INNER JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`
-WHERE `teachers`.`id`= 44
-;
+WHERE `teachers`.`id`= 44;
 
 -- 4.	Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`id`, `students`.`name`, `students`.`surname`, `students`.`date_of_birth`, `students`.`fiscal_code`, `students`.`enrolment_date`, `students`.`registration_number`, `students`.`registration_number`, `degrees`.`name`, `departments`.`name` 
+FROM `students`
+INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments` ON `degrees`.`department_id` = `departments`.`id` 
+ORDER BY `students`.`surname`, `students`.`name` ASC;
